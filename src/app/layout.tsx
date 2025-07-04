71e7740fc3142760cb08d7b7ae6afd9561bd9502
chang <liuchang85501@gmail.com>
2025-07-04 09:10:33 +0800

    feat:remove useless code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import BackButton from "@/components/BackButton";
 import DynamicMap from "@/components/DynamicMap";
 import { trails } from "@/data/trail-markers";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -34,9 +23,7 @@ export default function RootLayout({
   ];
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <div className="flex flex-col h-screen overflow-hidden bg-chang">
           <nav className="w-full p-4 flex items-center justify-center sticky top-0 z-10">
             <BackButton />
